c1aca9cbf44a6a1b699775a5d4ca20121fd6dfde
mofdi <mofdi.riahi@gmail.com>
2020-01-20 16:40:10 +0100

    BUWDI-569 update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    "gatsby-image",
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
